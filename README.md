7b74b84b4e25d7c8d4b231f14687d7b65e8ae640
ayemin-tun <76741291+ayemin-tun@users.noreply.github.com>
2023-09-03 19:35:11 +0630

    modify project folder

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # profilo
+
+This is a profilo website using HTML, Css, Jquery, Bootstrap
---
+++ core/javascript/function.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $("#flexSwitchCheckDefault").change(function () {
+    if ($(this).is(":checked")) {
+      $("#moon").css("display", "none");
+      $("#sun").css("display", "block");
+    } else {
+      $("#moon").css("display", "block");
+      $("#sun").css("display", "none");
+    }
+  });
+});
+
+$(document).ready(function () {
+  $("#downloadCVBtn").click(function () {
+    var fileURL = "additonal_file/Personal-Resume-Aye-MIn-Tun (for dev).docx";
+    var a = $("<a>")
+      .attr("href", fileURL)
+      .attr("download", "Personal Resume for Aye Min Tun")
+      .appendTo("body");
+    a[0].click();
+    a.remove();
+  });
+});
